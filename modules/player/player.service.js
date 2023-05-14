24826a232d1e1aa77ada2c876486b5d6e56a2c02
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2023-05-14 03:27:26 +0200

    Only registering players with unique name

--- modules/player/player.service.js
+++ modules/player/player.service.js
@@ -4,6 +4,13 @@ class PlayerService {
     }
 
     async addUser(player) {
+        const existingUser = await global.connection.promise().query("SELECT name FROM PlayerTable WHERE name = ?", [player]);
+        console.log(existingUser[0].length);
+        if (existingUser[0].length > 0) {
+            console.log('User already exists');
+            return; // or throw an error if desired
+        }
+
         return await global.connection.promise().query("INSERT INTO PlayerTable (name) VALUES (?)", [player]);
     }
 }
